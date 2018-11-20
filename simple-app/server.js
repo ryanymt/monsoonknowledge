13ab39bb05a488253ebcac476e65b7de0d2982ee
student <student@gogs.com>
2018-11-20 14:01:02 +0800

    123

--- simple-app/server.js
+++ simple-app/server.js
@@ -6,12 +6,13 @@ var config = require('./load-config.js');
 //console.log(config.service.context);
 
 app.set('port', process.env.PORT || 8080);
+app.set('public_url', process.env.PUBLIC_URL || '127.0.0.1');
 
 app.get('/simple-app/'+config.service.context, (req, res) => {
-    res.send('Hello World ' + config.service.response)
+    res.send('Response coming from :: ' + config.service.response)
 })
 
-app.listen(app.get('port'));
+app.listen(app.get('port'), app.get('public_url'));
 
 //app.listen(5656, () => {
 //   console.log('http://localhost:5656')
